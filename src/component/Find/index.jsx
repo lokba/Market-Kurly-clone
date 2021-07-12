efd8ce5dfeccda9850eaa1ac2b92ec99d8faa48f
sanglok <evertree6031@gmail.com>
2021-07-12 18:40:08 +0900

    Revert "Feature : 아이디,비밀번호 찾기 상태값 저장하기"
    This reverts commit 16613b961120b8166947c46e6ab869da94886604.

--- src/component/Find/index.jsx
+++ src/component/Find/index.jsx
@@ -5,8 +5,7 @@ import SideMenu from '../common/SideMenu/index';
 import { FindBox } from './styles';
 
 
-const Find = ({ param, onChange, findInfo }) => {
-    const { username, email, userId } = findInfo;
+const Find = ({ param }) => {
     return (
         <FindBox>
             <HeaderContainer />
@@ -18,18 +17,18 @@ const Find = ({ param, onChange, findInfo }) => {
                 <div className="form_box">
                     <form className="write_form">
                         <div className="form_tit">이름</div>
-                        <input onChange={onChange} value={username} name="username" placeholder="고객님의 이름을 입력해주세요" />
+                        <input placeholder="고객님의 이름을 입력해주세요" />
                         {
                             param === "find_pwd" &&
                             (
                                 <>
                                     <div className="form_tit upper">아이디</div>
-                                    <input onChange={onChange} value={userId} name="userId" placeholder="고객님의 아이디를 입력해주세요" />
+                                    <input placeholder="고객님의 아이디를 입력해주세요" />
                                 </>
                             )
                         }
                         <div className="form_tit upper">이메일</div>
-                        <input onChange={onChange} value={email} name="email" placeholder="가입 시 등록하신 이메일 주소를 입력해주세요" />
+                        <input placeholder="가입 시 등록하신 이메일 주소를 입력해주세요" />
                         <div className=" btnBox">
                             <a href>
                                 <span className="loginBtn">
--- src/container/FindContainer.js
+++ src/container/FindContainer.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import { useState } from 'react';
 import { withRouter } from 'react-router';
 import Find from '../component/Find/index';
 
 const FindContainer = ({ match }) => {
     const { find: find_param } = match.params;
 
-    const [findInfo, setFindInfo] = useState({
-        username: "",
-        email: "",
-        userId: "",
-    });
-
-    const onChange = (e) => {
-        const { name, value } = e.target;
-        setFindInfo({
-            ...findInfo,
-            [name]: value,
-        })
-    }
-
     return (
-        <Find
-            param={find_param}
-            onChange={onChange}
-            findInfo={findInfo}
-        />
+        <Find param={find_param} />
     );
 };
 
