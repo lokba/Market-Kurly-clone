0f17495e8aea36b54279ed9ce5654493401eed7e
sanglok <evertree6031@gmail.com>
2021-07-13 10:39:18 +0900

    Feature : 아이디,비밀번호 찾기 상태값 저장하기

--- src/container/FindContainer.js
+++ src/container/FindContainer.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 import { withRouter } from 'react-router';
 import Find from '../component/Find/index';
 
 const FindContainer = ({ match }) => {
     const { find: find_param } = match.params;
 
+    const [findInfo, setFindInfo] = useState({
+        username: "",
+        email: "",
+        userId: "",
+    });
+
+    const onChange = (e) => {
+        const { name, value } = e.target;
+        setFindInfo({
+            ...findInfo,
+            [name]: value,
+        })
+    }
+
+    console.log("findInfo", findInfo);
+
     return (
-        <Find param={find_param} />
+        <Find
+            param={find_param}
+            onChange={onChange}
+            findInfo={findInfo}
+        />
     );
 };
 
